5e773025b822ad21e6b390ae444c8a35832087cd
Rita <ugwuanyinnenna43@gmail.com>
2026-01-24 14:24:48 -0500

    provisioning kubernetes cluster in azure cloud

--- tf-value/4_subent.tf
+++ tf-value/4_subent.tf
@@ -1,4 +1,4 @@
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "subnet1" {
   name                 = "hipaa-subnet2"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
